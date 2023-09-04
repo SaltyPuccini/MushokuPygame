85f8ccd7cdcb99abce0f60da5d23c79528a5a4ab
Kajetan Wojciechowski <kkajetanwojciechowski@gmail.com>
2023-09-05 01:03:20 +0200

    Improve main character keyboard movement

--- EventsHandler.py
+++ EventsHandler.py
@@ -9,40 +9,47 @@ class EventsHandler:
 
     def __init__(self, game: Game):
         self.game = game
+        self.last_pressed = None
 
     def handle_events(self):
         events = pygame.event.get()
         for event in events:
-            if event.type == pygame.KEYDOWN and not self.game.level.map.is_animation_ongoing:
+            if event.type == pygame.KEYDOWN:
                 self.handle_menu_key_press_events(event)
-                self.handle_main_character_movement(event)
+                self.last_pressed = event.key
+            if event.type == pygame.KEYUP:
+                self.last_pressed = None
 
-    def handle_menu_key_press_events(self, event: Event):
+        if not self.game.level.map.is_animation_ongoing and self.last_pressed is not None:
+            self.handle_main_character_movement()
+
+    def handle_menu_key_press_events(self, event):
         if event.key == pygame.K_ESCAPE:
             self.game.gamestate = GameState.MAIN_MENU
-            print(self.game.gamestate)
         if event.key == pygame.K_q:
             self.game.gamestate = GameState.QUIT
 
-    def handle_main_character_movement(self, event: Event):
+    def handle_main_character_movement(self):
         animation_path = "Resources/Animation/"
         drawn_map = self.game.level.map
-        if event.key == pygame.K_DOWN:
+        key = self.last_pressed
+
+        if key == pygame.K_DOWN:
             animation_path += "WalkDown"
             drawn_map.animation_path = animation_path
             drawn_map.is_animation_ongoing = True
 
-        if event.key == pygame.K_UP:
+        if key == pygame.K_UP:
             animation_path += "WalkUp"
             drawn_map.animation_path = animation_path
             drawn_map.is_animation_ongoing = True
 
-        if event.key == pygame.K_RIGHT:
+        if key == pygame.K_RIGHT:
             animation_path += "WalkRight"
             drawn_map.animation_path = animation_path
             drawn_map.is_animation_ongoing = True
 
-        if event.key == pygame.K_LEFT:
+        if key == pygame.K_LEFT:
             animation_path += "WalkLeft"
             drawn_map.animation_path = animation_path
             drawn_map.is_animation_ongoing = True
